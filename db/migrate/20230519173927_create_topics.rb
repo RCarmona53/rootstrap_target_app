cf17d73d1498022fc5ed74fd00fcaabf380d9850
Rodrigo Carmona <donacionestpfinal@outlook.com>
2023-05-22 18:15:35 -0300

    Fix pr comments

--- db/migrate/20230519173927_create_topics.rb
+++ db/migrate/20230519173927_create_topics.rb
@@ -3,7 +3,9 @@ class CreateTopics < ActiveRecord::Migration[7.0]
     create_table :topics do |t|
       t.string :name, null: false, unique: true
 
-      t.timestamps
+      t.timestamps null: false
     end
+
+    add_index :topics, :name, unique: true
   end
 end
--- db/migrate/20230522135216_add_index_to_topics.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToTopics < ActiveRecord::Migration[7.0]
-  def change
-    add_index :topics, :name, unique: true
-  end
-end
