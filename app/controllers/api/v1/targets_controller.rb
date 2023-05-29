97ad077fdd005add83d32db46f6350129a651da4
Rodrigo Carmona <donacionestpfinal@outlook.com>
2023-05-29 10:44:58 -0300

    Targets in map

--- app/controllers/api/v1/targets_controller.rb
+++ app/controllers/api/v1/targets_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class TargetsController < Api::V1::ApiController
+      def index
+        @targets = policy_scope(current_user.targets)
+      end
+
       def create
         authorize Target
         @target = TargetService.new(current_user, resource_params).create!
