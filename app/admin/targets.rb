9c0bb95ff721acbadf8e3aa242a8c0de995f5ece
Rodrigo Carmona <donacionestpfinal@outlook.com>
2023-06-22 12:16:00 -0300

    admin target index

---
+++ app/admin/targets.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Target do
+  permit_params :title, :lat, :lng, :radius, :topic_id
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :radius, min: 0
+    column :lat
+    column :lng
+    column :topic
+    column :user
+    actions
+  end
+  filter :title
+  filter :radius
+  filter :lat
+  filter :lng
+  filter :topic
+  filter :user
+
+  controller do
+    def scoped_collection
+      super.includes(:topic, :user)
+    end
+  end
+end
---
+++ spec/admin/target_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin Targets', type: :request do
+  let(:admin_user) { FactoryBot.create(:admin_user) }
+
+  before do
+    sign_in admin_user
+  end
+
+  describe 'Show all targets' do
+    it 'displays all targets for the admin user' do
+      target1 = FactoryBot.create(:target, title: 'Target 1')
+      target2 = FactoryBot.create(:target, title: 'Target 2')
+
+      get '/admin/targets'
+
+      expect(response).to have_http_status(200)
+
+      expect(response.body).to include(target1.title)
+      expect(response.body).to include(target2.title)
+    end
+  end
+end
