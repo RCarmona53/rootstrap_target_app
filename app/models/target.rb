6322a39e6f2d7600c60e65affb36bf91bf4fb6d7
Rodrigo Carmona <donacionestpfinal@outlook.com>
2023-07-04 11:39:32 -0300

    comments fixes

--- app/models/target.rb
+++ app/models/target.rb
@@ -44,9 +44,7 @@ class Target < ApplicationRecord
   end
 
   def max_targets
-    return if user.vip?
-
-    return unless user.targets.count >= MAX_TARGETS
+    return unless !user.vip? && user.targets.count >= MAX_TARGETS
 
     errors.add(:user, I18n.t('api.errors.max_targets_reached', max_targets: MAX_TARGETS))
   end
