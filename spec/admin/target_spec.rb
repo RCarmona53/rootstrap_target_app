4cb41fa1ce549de4d7c3f71d72335892b039e761
Rodrigo Carmona <donacionestpfinal@outlook.com>
2023-07-12 20:05:05 -0300

    final commit

--- spec/admin/target_spec.rb
+++ spec/admin/target_spec.rb
@@ -9,11 +9,11 @@ RSpec.describe 'Admin Targets', type: :request do
 
   describe 'GET /admin/targets' do
     it 'filters targets by topic' do
-      topic1 = FactoryBot.create(:topic, name: 'Topic 1')
-      topic2 = FactoryBot.create(:topic, name: 'Topic 2')
+      topic1 = create(:topic, name: 'Topic 1')
+      topic2 = create(:topic, name: 'Topic 2')
 
-      target1 = FactoryBot.create(:target, title: 'Target 1', topic: topic1)
-      target2 = FactoryBot.create(:target, title: 'Target 2', topic: topic2)
+      target1 = create(:target, title: 'Target 1', topic: topic1)
+      target2 = create(:target, title: 'Target 2', topic: topic2)
 
       get '/admin/targets', params: { q: { topic_id_eq: topic1.id } }
 
@@ -23,8 +23,8 @@ RSpec.describe 'Admin Targets', type: :request do
     end
 
     it 'displays all targets for the admin user' do
-      target1 = FactoryBot.create(:target, title: 'Target 1')
-      target2 = FactoryBot.create(:target, title: 'Target 2')
+      target1 = create(:target, title: 'Target 1')
+      target2 = create(:target, title: 'Target 2')
 
       get '/admin/targets'
 
